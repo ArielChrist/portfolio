957efd8263a77b3d2601616130b48face48663b3
ArielChrist <archngato20@icloud.com>
2026-09-15 15:30:54 +0200

    correction 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,3 +11,4 @@ export default defineConfig({
     },
   },
 });
+
